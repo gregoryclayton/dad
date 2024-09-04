e43d938c5ccf6f8d25faae572d8fabfad8976f7a
density <44949218+gregoryclayton@users.noreply.github.com>
2024-09-03 22:10:04 -0400

    Add files via upload

---
+++ contentObj.js
@@ -0,0 +1,40 @@
+contentArray = {
+
+    "_1date" : "<i>January 6, 1993</i>",
+    "_1title" : "<b>The Blurst of Times</b>",
+    "_1content" :"this is the greatest test in the world  and a time ot rebuld the lost knowledge of agraraian times long past and forgotten a time to regain space and admiration",
+    "_1link1" : "https://media.istockphoto.com/id/1430537932/photo/blue-sky-with-white-clouds.webp?b=1&s=170667a&w=0&k=20&c=h9jXcgHg0keOp2OnI-hkJhsAEAEewKNediKrg-s97a8=",
+    "_1link2" : "https://media.istockphoto.com/id/1430537932/photo/blue-sky-with-white-clouds.webp?b=1&s=170667a&w=0&k=20&c=h9jXcgHg0keOp2OnI-hkJhsAEAEewKNediKrg-s97a8=",
+    "_1link3" : "https://media.istockphoto.com/id/1430537932/photo/blue-sky-with-white-clouds.webp?b=1&s=170667a&w=0&k=20&c=h9jXcgHg0keOp2OnI-hkJhsAEAEewKNediKrg-s97a8=",
+    "_1linklink1" : "https://www.google.com",
+    "_1linklink2" : "",
+    "_1linklink3" : "",
+
+
+
+    "_2date" : "<i>January 6, 441993</i>",
+    "_2title" : "<b>The Blur44st of Times</b>",
+    "_2content" :"covid cuaesese esf",
+    "_2link1" : "https://media.istockphoto.com/id/1430537932/photo/blue-sky-with-white-clouds.webp?b=1&s=170667a&w=0&k=20&c=h9jXcgHg0keOp2OnI-hkJhsAEAEewKNediKrg-s97a8=",
+    "_2link2" : "link12",
+    "_2link3" : "link234234",
+    "_2linklink1" : "",
+    "_2linklink2" : "",
+    "_2linklink3" : "",
+
+
+
+    "_3date" : "<i>January 6, 1993</i>",
+    "_3title" : "<b>The Blurst of Timesssssss</b>",
+    "_3content" :"this is the greatest test in the world  and a time ot rebuld the lost knowledge of agraraian times long past and forgotten a time to regain space and admiration",
+    "_3link1" : "https://media.istockphoto.com/id/1430537932/photo/blue-sky-with-white-clouds.webp?b=1&s=170667a&w=0&k=20&c=h9jXcgHg0keOp2OnI-hkJhsAEAEewKNediKrg-s97a8=",
+    "_3link2" : "https://media.istockphoto.com/id/1430537932/photo/blue-sky-with-white-clouds.webp?b=1&s=170667a&w=0&k=20&c=h9jXcgHg0keOp2OnI-hkJhsAEAEewKNediKrg-s97a8=",
+    "_3link3" : "https://media.istockphoto.com/id/1430537932/photo/blue-sky-with-white-clouds.webp?b=1&s=170667a&w=0&k=20&c=h9jXcgHg0keOp2OnI-hkJhsAEAEewKNediKrg-s97a8=",
+    "_3linklink1" : "",
+    "_3linklink2" : "",
+    "_3linklink3" : "",
+
+    "videolink" : "C:/Users/grego/Desktop/pros/lon3ly/cloudyNeg.mp4"
+
+};
+
